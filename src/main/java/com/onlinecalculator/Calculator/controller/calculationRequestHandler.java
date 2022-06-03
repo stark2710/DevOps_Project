129af20a20a05e3f1c6f15f575ea6dbd944f8375
I526663 <bharat.malviya@sap.com>
2022-06-03 19:27:10 +0530

    udates

--- src/main/java/com/onlinecalculator/Calculator/controller/calculationRequestHandler.java
+++ src/main/java/com/onlinecalculator/Calculator/controller/calculationRequestHandler.java
@@ -2,9 +2,11 @@ package com.onlinecalculator.Calculator.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/calculator")
+@RestController
+@RequestMapping("/calculator")
 public class calculationRequestHandler {
 
     @GetMapping("/calculate/{num1}/{operation}/{num2}")
